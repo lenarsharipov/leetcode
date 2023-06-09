e3279ebef0cb9c994dfa316e27dd4d5e1031dba2
lenarsharipov <lenarsharipov@gmail.com>
2023-06-09 14:14:59 +0300

    Upload Solution and Test for "1071. Greatest Common Divisor of Strings"

---
+++ src/test/java/leetcode/problems/L003Leetcode1431Test.java
@@ -0,0 +1,33 @@
+package leetcode.problems;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class L003Leetcode1431Test {
+    private final L003Leetcode1431 solution = new L003Leetcode1431();
+
+    @Test
+    void firstTest() {
+        List<Boolean> expected = List.of(true, true, true, false, true);
+        assertThat(expected)
+                .isEqualTo(solution.kidsWithCandies(new int[] {2, 3, 5, 1, 3}, 3));
+    }
+
+    @Test
+    void secondTest() {
+        List<Boolean> expected = List.of(true, false, false, false, false);
+        assertThat(expected)
+                .isEqualTo(solution.kidsWithCandies(new int[] {4, 2, 1, 1, 2}, 1));
+    }
+
+    @Test
+    void thirdTest() {
+        List<Boolean> expected = List.of(true, false, true);
+        assertThat(expected)
+                .isEqualTo(solution.kidsWithCandies(new int[] {12, 1, 12}, 10));
+    }
+
+}
