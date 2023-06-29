4cf9fb71617ab6f0ffe623c793a22f69a0365d3f
lenarsharipov <lenarsharipov@gmail.com>
2023-06-29 22:06:35 +0300

    Solution and test for "412. Fizz Buzz".

---
+++ src/test/java/com/leetcode/Leetcode412Test.java
@@ -0,0 +1,42 @@
+package com.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class Leetcode412Test {
+    private final Leetcode412 solution = new Leetcode412();
+
+    @Test
+    void firstTest() {
+        var n = 3;
+        var expected = List.of("1", "2", "Fizz");
+        assertThat(solution.fizzBuzz(n))
+                .usingRecursiveComparison()
+                .isEqualTo(expected);
+    }
+
+    @Test
+    void secondTest() {
+        var n = 5;
+        var expected = List.of("1", "2", "Fizz", "4", "Buzz");
+        assertThat(solution.fizzBuzz(n))
+                .usingRecursiveComparison()
+                .isEqualTo(expected);
+    }
+
+    @Test
+    void thirdTest() {
+        var n = 15;
+        var expected = List.of(
+                "1", "2", "Fizz", "4", "Buzz",
+                "Fizz", "7", "8", "Fizz", "Buzz",
+                "11", "Fizz", "13", "14", "FizzBuzz");
+        assertThat(solution.fizzBuzz(n))
+                .usingRecursiveComparison()
+                .isEqualTo(expected);
+    }
+
+}
