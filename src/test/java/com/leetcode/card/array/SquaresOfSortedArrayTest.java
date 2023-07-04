d2ca9114582ea338bbd16cddb249f06f834c769c
lenarsharipov <lenarsharipov@gmail.com>
2023-07-04 08:55:02 +0300

    Squares of a Sorted Array

---
+++ src/test/java/com/leetcode/card/array/SquaresOfSortedArrayTest.java
@@ -0,0 +1,24 @@
+package com.leetcode.card.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class SquaresOfSortedArrayTest {
+    private final SquaresOfSortedArray solution = new SquaresOfSortedArray();
+
+    @Test
+    void firstTest() {
+        final int[] nums = {-4, -1, 0, 3, 10};
+        int[] expected = {0, 1, 9, 16, 100};
+        assertThat(solution.sortedSquares(nums)).isEqualTo(expected);
+    }
+
+    @Test
+    void secondTest() {
+        final int[] nums = {-7, -3, 2, 3, 11};
+        int[] expected = {4, 9, 9, 49, 121};
+        assertThat(solution.sortedSquares(nums)).isEqualTo(expected);
+    }
+
+}
