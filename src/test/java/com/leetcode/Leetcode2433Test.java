b6b22a8a8fde9927db05de4c15082fc8fcdcf2b0
TETACOMTR <cagri@teta.com.tr>
2023-11-15 10:36:10 +0300

    Solution of Leetcode2433

---
+++ src/test/java/com/leetcode/Leetcode2433Test.java
@@ -0,0 +1,23 @@
+package com.leetcode;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class Leetcode2433Test {
+    private final Leetcode2433 solution = new Leetcode2433();
+
+    @Test
+    public void test1() {
+        int[] pref = {5, 2, 0, 3, 1};
+        int[] output = {5, 7, 2, 3, 2};
+        assertThat(output).isEqualTo(solution.findArray(pref));
+    }
+
+    @Test
+    public void test2() {
+        int[] pref = {13};
+        int[] output = {13};
+        assertThat(output).isEqualTo(solution.findArray(pref));
+    }
+
+}
