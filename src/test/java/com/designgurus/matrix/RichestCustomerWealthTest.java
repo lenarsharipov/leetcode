a834c85d42bb83732e789dec8cd5b3f4c71eaf45
TETACOMTR <cagri@teta.com.tr>
2023-11-20 09:55:54 +0300

    Design Gurus - Matrix - Solution and tests of RichestCustomerWealth

---
+++ src/test/java/com/designgurus/matrix/RichestCustomerWealthTest.java
@@ -0,0 +1,44 @@
+package com.designgurus.matrix;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RichestCustomerWealthTest {
+
+    @Test
+    public void test1() {
+        int[][] accounts = {
+                {5, 2, 3},
+                {0, 6, 7}
+        };
+        int expected = 13;
+        assertThat(RichestCustomerWealth.maximumWealth(accounts))
+                .isEqualTo(expected);
+    }
+
+    @Test
+    public void test2() {
+        int[][] accounts = {
+                {1, 2},
+                {3, 4},
+                {5, 6}
+        };
+        int expected = 11;
+        assertThat(RichestCustomerWealth.maximumWealth(accounts))
+                .isEqualTo(expected);
+    }
+
+    @Test
+    public void test3() {
+        int[][] accounts = {
+                {10, 100},
+                {200, 20},
+                {30, 300}
+        };
+        int expected = 330;
+        assertThat(RichestCustomerWealth.maximumWealth(accounts))
+                .isEqualTo(expected);
+    }
+
+}
