d069979a456136ae519a35c8c838509649f97059
lenarsharipov <lenarsharipov@gmail.com>
2024-01-28 06:00:39 +0300

    BinaryTree - Recursive PreOrder of BinaryTree

--- src/main/java/com/algos/tree/BinaryTree.java
+++ src/main/java/com/algos/tree/BinaryTree.java
@@ -4,7 +4,7 @@ public class BinaryTree {
     private TreeNode root;
 
     public void createBinaryTree() {
-        TreeNode first = new TreeNode(1);
+        TreeNode first = new TreeNode(9);
         TreeNode second = new TreeNode(2);
         TreeNode third = new TreeNode(3);
         TreeNode fourth = new TreeNode(4);
@@ -17,9 +17,21 @@ public class BinaryTree {
         second.left = fourth;
         second.right = fifth;
 
+        System.out.println(root);
+
     }
 
-    private class TreeNode {
+    public void preOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        System.out.println(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    private static class TreeNode {
         private int data;
         private TreeNode left;
         private TreeNode right;
@@ -28,9 +40,20 @@ public class BinaryTree {
             this.data = data;
         }
 
+        @Override
+        public String toString() {
+            return "TreeNode{" +
+                    "data=" + data +
+                    ", left=" + left +
+                    ", right=" + right +
+                    '}';
+        }
     }
 
     public static void main(String[] args) {
+        var tree = new BinaryTree();
+        tree.createBinaryTree();
+        tree.preOrder(tree.root);
 
     }
 }
